c353faa95531a1eab06feb301b618f199cbd1881
mikk357 <mikk357@yandex.com>
2020-08-30 15:19:07 +0300

    update • separators • update.bat fix • requirements

--- update.bat
+++ update.bat
@@ -1,10 +1,10 @@
+@ REM script for windows, achtung!
+@ SET ORIGIN=%CD%
+@ CD /D %~dp0
 
-@SET T=%CD%
-@CD %~dp0
+@ CALL .venv\Scripts\activate.bat
+@ CALL python main.py
+@ CALL .venv\Scripts\deactivate.bat
 
-@CALL .venv\Scripts\activate.bat
-@CALL python main.py
-@CALL .venv\Scripts\deactivate.bat
-
-@CD %T%
-@ECHO OK
+@ CD /D %ORIGIN%
+@ ECHO     UwU    
